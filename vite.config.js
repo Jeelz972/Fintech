6b6c20abdc0176b281d7ef14dad4b6f59f7b2074
Jeelz972 <jeelz@hotmail.fr>
2025-11-27 13:00:28 +0100

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/finance-app/', // Remplacer par le nom de ton repo
+  base: '/Fintech/', // Remplacer par le nom de ton repo
 })
